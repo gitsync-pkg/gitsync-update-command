c1da5477744d84256535c2e94923859e93503ba1
twinh <twinhuang@qq.com>
2019-09-11 18:15:58 +0800

    fix: update command fail

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
+import * as fs from 'fs';
 import {Arguments, CommandModule} from 'yargs';
 import Sync from '@gitsync/sync';
-import {Config} from '@gitsync/config';
+import {Config, ConfigRepo} from '@gitsync/config';
 
 interface UpdateArguments extends Arguments {
   sourceDir: string
@@ -25,19 +26,28 @@ command.handler = async (argv: UpdateArguments) => {
   const config = new Config();
   config.checkFileExist();
 
-  const target: string = config.getRepoBySourceDir(argv.sourceDir);
+  const repo: ConfigRepo = config.getRepoBySourceDir(argv.sourceDir);
+  const target = config.getRepoDir(repo.target);
+  if (!fs.existsSync(target)) {
+    // TODO clone if not exists
+    throw new Error(`Target ${repo.target}, directory ${target} does not exists`);
+  }
 
   const cwd = process.cwd();
   process.chdir(target);
 
+  if (!repo.targetDir) {
+    repo.targetDir = '.';
+  }
+  [repo.sourceDir, repo.targetDir] = [repo.targetDir, repo.sourceDir];
+  repo.target = cwd;
+  console.log('repo', repo);
+
   const sync = new Sync();
-  await sync.sync({
+  await sync.sync(Object.assign({
     $0: '',
-    _: [],
-    target: cwd,
-    sourceDir: '.',
-    targetDir: argv.sourceDir,
-  });
+    _: []
+  }, repo));
 
   process.chdir(cwd);
 }
