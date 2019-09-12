81acd772a16520f911789532baf50bbce8fa0055
twinh <twinhuang@qq.com>
2019-09-12 10:40:05 +0800

    refactor: add getRepoDirByRepo, remove getRepoDir

--- index.ts
+++ index.ts
@@ -27,21 +27,16 @@ command.handler = async (argv: UpdateArguments) => {
   config.checkFileExist();
 
   const repo: ConfigRepo = config.getRepoBySourceDir(argv.sourceDir);
-  const target = config.getRepoDir(repo.target);
-  if (!fs.existsSync(target)) {
-    // TODO clone if not exists
-    throw new Error(`Target ${repo.target}, directory ${target} does not exists`);
-  }
+  const repoDir = await config.getRepoDirByRepo(repo, true);
 
   const cwd = process.cwd();
-  process.chdir(target);
+  process.chdir(repoDir);
 
   if (!repo.targetDir) {
     repo.targetDir = '.';
   }
   [repo.sourceDir, repo.targetDir] = [repo.targetDir, repo.sourceDir];
   repo.target = cwd;
-  console.log('repo', repo);
 
   const sync = new Sync();
   await sync.sync(Object.assign({
