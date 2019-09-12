654bf08f5a0d00b49779cab66290ddcf4ac8b11d
twinh <twinhuang@qq.com>
2019-09-13 00:10:53 +0800

    test: do not disableColor, make it easier to debug

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,10 +1,6 @@
 import * as fs from 'fs';
-import {createRepo, removeRepos, disableColor, runCommand} from '@gitsync/test';
+import {createRepo, removeRepos, runCommand} from '@gitsync/test';
 import update from '..';
-
-beforeAll(() => {
-  disableColor();
-});
 
 afterAll(() => {
   removeRepos();
